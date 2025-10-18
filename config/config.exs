6f5478340e275a2e0a677fe55d13ebd5a0909035
Fabricio Futryk <ffutryk52@gmail.com>
2025-10-18 04:49:20 -0300

    chore: add tz dependency and timezone database configuration

--- config/config.exs
+++ config/config.exs
@@ -3,6 +3,8 @@
 
 import Config
 
+config :elixir, :time_zone_database, Tz.TimeZoneDatabase
+
 config :advent_of_code, Advent.Input,
-  #allow_network?: true,
+  # allow_network?: true,
   session_cookie: System.get_env("ADVENT_OF_CODE_SESSION_COOKIE")
